672be3851d1e68c6740adc66855a496be63a2643
DVMaja <majadreilinger@gmail.com>
2023-02-01 15:21:31 +0100

    jatekos, gep lista 21 folott vereseg

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # megoldás
 
-def eredmeny():
-    pass
+jatekos_pontjai = [10, 3, 4]
+gep_pontjai = [3, 5, 2]
+def eredmeny(jatekos_pontjai, gep_pontjai):
+    if jatekos_pontjai > 21:
+        print("Játékos vesztett!")
+    elif gep_pontjai > 21:
+        print("Gép vesztett!")
 
 # teszt esetek
