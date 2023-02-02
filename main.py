dec60f06c2e041b15ed2bd8ecc7452cca4e3fdfc
DVMaja <majadreilinger@gmail.com>
2023-02-02 19:47:30 +0100

    pontszamitas metodus jo, meg nemir ki semmit

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # megoldás
 
-def eredmeny(osszeg):
-    pontszamitas(osszeg)
-    if osszeg > 21:
+jatekos_lapok = [2, 3, 4]
+gep_lapok = [10, 4, 10]
+# nem érem el jelenleg ezeket
+# ezért nem látok kiírva semmit
+
+
+def eredmeny(jatekos_lapok: [int], gep_lapok: [int], osszeg):
+    jatekos_pontok: [int] = pontszamitas(osszeg)
+    gep_pontok: [int] = pontszamitas(osszeg)
+
+    if jatekos_pontok > 21:
         print("Játékos vesztett!")
-    elif osszeg > 21:
+    elif gep_pontok > 21:
         print("Gép vesztett!")
+    else:
+        print("LEfutott")
 
-
-def pontszamitas(lista):
+# valahogy érd el a
+def pontszamitas(lapok: [list]):
     osszeg = 0
-    for i in range(lista):
-        osszeg = osszeg + lista[i]
+    for i in range(len(lapok)):
+        osszeg = osszeg + lapok[i]
     return osszeg
 
 # teszt esetek
